f402c4b5761543768e7888916462836fd6fad2ef
Sandro Cunha <sandromedeirosdacunha@gmail.com>
2023-07-12 16:37:00 -0300

    Terminado

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoButtonDeletAllComponent } from './components/todo-button-delet-all/todo-button-delet-all.component';
+import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    TodoButtonDeletAllComponent,
+    TodoInputAddItensComponent,
+    TodoListComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports:[
+    HomeComponent
+  ]
+})
+export class HomeModule { }
